fe2d279ab75b85304891f85e09697b6a1365dc0c
Ben Scholer <benscholer3248511@gmail.com>
2016-08-18 17:41:49 -0400

    Added basic mod files and item.lua in prototypes/

---
+++ prototypes/item.lua
@@ -0,0 +1,9 @@
+data:extend({
+
+    {
+        type    =   "item",
+        name    =   "Selling Chest",
+        icon    =   "__trading__/graphics/icon_selling.png",
+        flags   =   "goes-to-quickbar",
+        subgroup=   "logistics",
+
