f0225f8d754f9526762fc63900dfad46837ef291
Ben Scholer <benscholer3248511@gmail.com>
2016-08-20 13:10:31 -0400

    Completed recipe.lua and item.lua

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -2,8 +2,20 @@ data:extend({
 
     {
         type    =   "item",
-        name    =   "Selling Chest",
+        name    =   "trading-chest-sell",
         icon    =   "__trading__/graphics/icon_selling.png",
         flags   =   "goes-to-quickbar",
-        subgroup=   "logistics",
-
+        subgroup    =   "logistics",
+        place_result    =   "Selling Chest",
+        stack_size  =   50,
+    },
+    {
+        type    =   "item",
+        name    =   "trading-chest-buy",
+        icon    =   "__trading__/graphics/icon_buying.png",
+        flags   =   "goes-to-quickbar",
+        subgroup    =   "logistics",
+        place_result    =   "Buying Chest",
+        stack_size  =   50,
+    }
+})
---
+++ prototypes/recipe.lua
@@ -0,0 +1,24 @@
+data:extend({
+    {
+        type    =   "recipe",
+        name    =   "trade-chest-buy",
+        enabled =   "false",
+        ingredients =
+        {
+            {"smart-chest", 1},
+            {"electronic-circuit", 5}
+        },
+        result  =   "trade-chest-buy"
+    },
+    {
+        type    =   "recipe",
+        name    =   "trade-chest-sell"
+        enabled =   "false",
+        ingredients =
+        {
+            {"smart-chest", 1},
+            {"electronic-circuit", 5}
+        },
+        result  =   "trade-chest-sell"
+    }
+})
